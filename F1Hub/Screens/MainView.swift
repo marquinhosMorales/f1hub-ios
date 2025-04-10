762d9389ea7e5a6f021a5c24635dcb622b8ea1db
Marcos Morales <marquinhosm@outlook.com>
2025-04-09 21:14:26 -0300

    Create the project

---
+++ F1Hub/Screens/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  F1HubApp.swift
+//  F1Hub
+//
+//  Created by Marcos Morales on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MainView: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ F1Hub/Utils/Color+Palette.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Palette.swift
+//  F1Hub
+//
+//  Created by Marcos Morales on 09/04/2025.
+//
+
+import SwiftUICore
+
+extension Color {
+    static var backgroundColor: Color {
+        return Color("BackgroundColor")
+    }
+}
